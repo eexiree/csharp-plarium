317af976f54eb9d43fb74853594adb5557a24388
eexiree <qrampagep@gmail.com>
2021-10-23 19:13:12 +0300

    4th Lesson

--- Lesson_4/Task C/Program.cs
+++ Lesson_4/Task C/Program.cs
@@ -28,19 +28,19 @@ namespace Clock_Shop
         {            
             #region Вывести марки заданного типа часов.
             WriteLine("Вывести информацию о механических часах, цена на которые не превышает заданную\n\nВведите тип часов\n1:Quartz\t2:Mechanical\n");
-            while(byte.TryParse(ReadLine(), out byte input) && (input != 1 || input != 2))
-            {
-                ClockShop.BrandByClockType((ClockType)input - 1);
-            }
+            byte input;
+            while (!(byte.TryParse(ReadLine(), out input) && (input == 1 || input == 2)));
+            ClockShop.BrandByClockType((ClockType)input - 1);
+            Console.ReadKey();
             Clear();
             #endregion
 
             #region Вывести информацию о механических часах, цена на которые не превышает заданную
             WriteLine("Вывести информацию о механических часах, цена на которые не превышает заданную\n\nВведите цену:\n");
-            while (decimal.TryParse(ReadLine(), out decimal cost))
-            {
-                ClockShop.MechanicalClocksInfo(cost);
-            }
+            decimal cost;
+            while (!decimal.TryParse(ReadLine(), out cost));
+            ClockShop.MechanicalClocksInfo(cost);
+            Console.ReadKey();
             Clear();
             #endregion
 
@@ -48,19 +48,18 @@ namespace Clock_Shop
             WriteLine("Вывести марки часов, изготовленных в заданной стране\n\nВведите страну\n");
             ClockShop.AvailableCountries(true);
             string country;
-            while(ClockShop.AvailableCountries().Contains(country = ReadLine()))
-            {
-                ClockShop.BrandByCountry(country);
-            }
+            while (!ClockShop.AvailableCountries().Contains(country = ReadLine()));
+            ClockShop.BrandByCountry(country);
+            Console.ReadKey();
             Clear();
             #endregion
 
             #region Вывести производителей, общая сумма часов которых в магазине не превышает заданную
             WriteLine("\nВывести производителей, общая сумма часов которых в магазине не превышает заданную\nВведите общую цену часов в магазине:");
-            while (decimal.TryParse(ReadLine(), out decimal totalCost))
-            {
-                ClockShop.ProducersByTotalCost(totalCost);
-            }
+            decimal totalCost;
+            while (!decimal.TryParse(ReadLine(), out totalCost) == false);
+            ClockShop.ProducersByTotalCost(totalCost);
+            Console.ReadKey();
             Clear();
             #endregion
 
